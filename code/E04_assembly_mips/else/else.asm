7ee54d19d64304a379b69cefe901a7f3e0fc7118
Andrea Canidio <5849153+acanidio@users.noreply.github.com>
2018-04-09 13:02:26 +0200

    Updated if, else and while examples.
    Signed-off-by: Andrea Canidio <5849153+acanidio@users.noreply.github.com>

--- code/E04_assembly_mips/else/else.asm
+++ code/E04_assembly_mips/else/else.asm
@@ -7,7 +7,7 @@
 	sgt	$s2, $s0, $s1
 	sgt	$s3, $s1, $zero
 	and	$s2, $s2, $s3
-	bne	$s2, $zero, else
+	beq	$s2, $zero, else
 	add	$s4, $s0, $s1
 	j	end
 else:	sub	$s4, $s0, $s1
--- code/E04_assembly_mips/if/if.asm
+++ code/E04_assembly_mips/if/if.asm
@@ -5,6 +5,6 @@
 	addi	$s1, $zero, 3
 	add	$s0, $s0, $s1
 	sgt	$s2, $s0, $s1
-	bne	$s2, $zero, end
+	beq	$s2, $zero, end
 	add	$s1, $s1, $s0
 end:	
